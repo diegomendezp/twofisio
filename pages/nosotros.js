12e342cdfc8dc409f42ab00e60b4d3aba5b96d3f
diegomendezp <diegomen15@gmail.com>
2019-03-08 09:14:11 +0100

    Team completed

---
+++ pages/nosotros.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import team from '../content/team.json'
+
+function nosotros(props) {
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  )
+}
+
+
+
+export default nosotros
+
