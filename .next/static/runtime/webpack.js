5a35c16ed59f2bd5a26d3edb25947c64b08760f4
diegomendezp <diegomen15@gmail.com>
2019-04-25 19:57:17 +0200

    Before merge Alberto's changes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48d3174115b3ec913e7b";
+/******/ 	var hotCurrentHash = "55f6b1ce386a96e32b43";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
