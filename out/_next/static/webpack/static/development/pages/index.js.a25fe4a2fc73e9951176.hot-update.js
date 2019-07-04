5366bef95df46c70c97979ce7966cc3010648418
diegomendezp <diegomen15@gmail.com>
2019-07-04 17:11:15 +0200

    First deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.a25fe4a2fc73e9951176.hot-update.js
@@ -0,0 +1,25 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./PagesStyles/FontStyle/FontBold.js":
+/*!*******************************************!*\
+  !*** ./PagesStyles/FontStyle/FontBold.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+
+var FontBold = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].h2.withConfig({
+  displayName: "FontBold",
+  componentId: "sc-5b97yu-0"
+})(["font-family:\"Monserrat-Semibold\";color:", ";font-size:1.5rem;letter-spacing:0.04rem;@media only screen and (min-width:769px) and (max-width:1440px){}@media only screen and (min-width:1441px){}"], function (props) {
+  return props.color === 'white' ? 'white' : '#740a64';
+});
+/* harmony default export */ __webpack_exports__["default"] = (FontBold);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.a25fe4a2fc73e9951176.hot-update.js.map
